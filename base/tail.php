baebb2a119d5eb024ba8697e6e4b268e397e4a3d
dlaenghks <73481538+dlaenghks@users.noreply.github.com>
2026-01-06 18:56:52 +0900

    Add files via upload

---
+++ base/tail.php
@@ -0,0 +1,39 @@
+<script charset="UTF-8" class="daum_roughmap_loader_script" src="https://ssl.daumcdn.net/dmaps/map_js_init/roughmapLoader.js"></script>
+
+    <script src="/base/script.js"></script>
+
+    <footer class="footer-section">
+        <div class="footer-container">
+            <div class="footer-logo">
+                <a href="/">
+                    <img src="/base/logo.png" alt="Logo">
+                </a>
+            </div>
+            <article class="footer-info">
+                <address>
+                    <span>연세우리아동발달클리닉</span>
+                    <span>경기 오산시 경기대로 178</span>
+                    <span>대표 이철희</span>
+                    <span>이메일 yonsei_childclinic@naver.com</span>
+                    <!--<br> <span>사업자등록번호 123-95-32475</span>--!>
+                    <span>전화 031-376-9636, 010-3605-9637</span>
+                </address>
+                <p class="footer-copyright">
+                    <b>Copyright &copy; Woori Kids Clinic. All rights reserved.</b> 
+                </p>
+            </article>
+        </div>
+    </footer>
+
+
+<script type="text/javascript" src="//wcs.pstatic.net/wcslog.js"></script>
+<script type="text/javascript">
+if(!wcs_add) var wcs_add = {};
+wcs_add["wa"] = "171df0fa33a7570";
+if(window.wcs) {
+  wcs_do();
+}
+</script>
+
+
+</body>
